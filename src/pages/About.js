9be9c0e5d2ae1af8ced099b2a8189ce16070a410
virtual_epoch <76543678+virtualepoch@users.noreply.github.com>
2022-07-17 20:26:43 -0600

    content edit

--- src/pages/About.js
+++ src/pages/About.js
@@ -11,11 +11,12 @@ export default function About() {
         <p>&gt; Daily real-time updates</p>
         <p>&gt; Pixel-perfection</p>
         <p>&gt; Responsive layouts</p>
-        <p>&gt; Static pages coded with HTML, CSS, and JavaScript</p>
-        <p>&gt; Dynamic apps created with React.js</p>
-        <p>&gt; Guarantee all code follows current W3C Standards and Guidelines</p>
+        <p>&gt; Dynamic apps coded with ReactJS</p>
+        <p>&gt; Static webpages coded with HTML, CSS, and JavaScript</p>
         <p>&gt; Search Engine Optimization</p>
-        <p className="inquire-statement">If you'd like to inquire into our services please press the 'Contact' link on the next page. Thanks for the visit!</p>
+        <p>&gt; Code that passes all W3C Validations</p>
+        <p className="inquire-statement">We guarantee everything we do. We never conclude any project until all the requirements of that project are fully completed. We are not happy until our clients' needs are fulfilled.</p>
+        <p className="inquire-statement"> If you'd like to inquire into our services please press the 'Contact' link on the next page. Thanks for the visit!</p>
 
         <div className="pyramid-container">
           <div className="pyramid-wrapper">
